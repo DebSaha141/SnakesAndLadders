f780dc6271653d4c5cf32a751e18f0591df23a9a
DebSaha141 <debsaha141@gmail.com>
2024-05-15 02:54:00 +0530

    Grid Fixed

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const board = document.querySelector("#board");
-
-for (let i = 1; i <= 100; i++) {
+let c = 0;
+let ch = [90, 70, 50, 30, 10];
+let noch = [100, 80, 60, 40, 20]
+let bool = false;
+for (let i = 100; i > 0; i--) {
     let boardsq = document.createElement("div");
-    boardsq.setAttribute("id", `${i}`);
+    if (ch.includes(i)) {
+        bool = true;
+    }
+    if (noch.includes(i)) {
+        bool = false;
+        c = 0;
+    }
+    if (bool === false) {
+        boardsq.setAttribute("id", `${i}`);
+    }
+    if (bool === true) {
+        if (c === 0) {
+            a = i - 9;
+            c++;
+        }
+        boardsq.setAttribute("id", `${a}`);
+        a++;
+    }
     boardsq.classList.add("format");
     board.append(boardsq);
 }
+
+let divBoard = document.getElementById("1");
+console.log(divBoard);
+
+divBoard.style.backgroundColor = "red";
